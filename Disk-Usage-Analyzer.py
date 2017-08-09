70fcea0b124019bce6d347f0d56f7d6ba16caa09
Abhay_B <kudosabhay@gmail.com>
2017-08-09 06:13:02 +0000

    Analyze Disk Usage and Email automatically

--- Disk-Usage-Analyzer.py
+++ Disk-Usage-Analyzer.py
@@ -6,7 +6,7 @@ df_output_lines = [s.split() for s in os.popen("df -Ph").read().splitlines()]
 #print(df_output_lines[1:])
 
 #Store a Threshold Value for 90% in a variable
-threshold = float(30)/100;
+threshold = float(90)/100;
 #print(threshold)
 
 #Measure the Length of Output obtained from the command 'df -h'
@@ -26,13 +26,13 @@ for i in range(array_length):
         #Check if this value is > 90%
         if(temp > threshold):
             conn = http.client.HTTPSConnection("maker.ifttt.com")
-            payload = "{\n\t\"value1\" : \"abhay@promethean-power.com\",\n\t\"value2\" : \"abhay.bhosale38@gmail.com\",\n\t\"value3\" : \"Usage of your Disk has exceeded more than 90% on your EC2 Instance\"\n}"
+            payload = "{\n\t\"value1\" : \"yourEmailID@youremail.com\",\n\t\"value2\" : \"cctoSomeone@email.com\",\n\t\"value3\" : \"Usage of your Disk has exceeded more than 90% on your EC2 Instance\"\n}"
             headers = {
             'content-type': "application/json",
             'cache-control': "no-cache",
             #'postman-token': "1e31a613-7d04-4b04-b663-1b38179cd3f6"
             }
-            conn.request("POST", "/trigger/Disk_Usage_Exceeded/with/key/pb6iH8iLldXveE6cp4EgA9eJhAvW6CwGzL4q77mI-iR", payload, headers)
+            conn.request("POST", "/trigger/your_IFTTT_Trigger/with/key/your_IFTTT_Key", payload, headers)
             res = conn.getresponse()
             data = res.read()
             print(data.decode("utf-8"))
